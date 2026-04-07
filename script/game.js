93fdcf54ab8acdae317c10b5b50046de17cce0b6
Aliaqa Alizada <alimhd276@gmail.com>
2026-04-07 09:47:35 +0200

    added new feature to enhance user experience

--- script/game.js
+++ script/game.js
@@ -1,16 +1,13 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new moveableObject();
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    character.src = 'img/2_character_pepe/2_walk/W-21.png';   
 
 
-    setTimeout(() => {
-          ctx.drawImage(character, 20, 20, 50, 140);
-    }, 100);
+    console.log('My Charchter', character);
 
 }
     
